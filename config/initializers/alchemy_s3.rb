801e3797325f95711a4cd91f1e91ee39c4a17941
Robin Böning <rb@magiclabs.de>
2014-05-03 21:40:45 +0200

    Adds dragonfly s3 app for attachments.

--- config/initializers/alchemy_s3.rb
+++ config/initializers/alchemy_s3.rb
@@ -1,10 +1,21 @@
 require 'dragonfly/s3_data_store'
 
-app_s3 = Dragonfly.app(:alchemy_pictures)
-app_s3.configure_with(:imagemagick)
-app_s3.configure do
+pictures_app = Dragonfly.app(:alchemy_pictures)
+pictures_app.configure_with(:imagemagick)
+pictures_app.configure do
   datastore :s3,
-    bucket_name: 'my_app',
+    bucket_name: 'alchemy-cms-pictures',
+    access_key_id: 'xxx',
+    secret_access_key: 'xxx',
+    region: 'eu-west-1',
+    storage_headers: {'x-amz-acl' => 'public-read'},
+    url_scheme: 'https'
+end
+
+attachments_app = Dragonfly.app(:alchemy_attachments)
+attachments_app.configure do
+  datastore :s3,
+    bucket_name: 'alchemy-cms-attachments',
     access_key_id: 'xxx',
     secret_access_key: 'xxx',
     region: 'eu-west-1',
